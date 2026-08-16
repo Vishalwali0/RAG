8cd68549dd0d4eb4225781082bb1b56a1ff8be8c
vishal <vishalwali0000@gmail.com>
2026-08-16 16:31:48 +0530

    update app

--- src/app.py
+++ src/app.py
@@ -1,16 +1,25 @@
-from src.data_loader import load_all_documents
-from src.vectorstore import FaissVectorStore
-from src.search import RAGSearch
+from data_loader import load_all_documents
+from vectorstore import FaissVectorStore
+from search import RAGSearch
+
 
 # Example usage
 if __name__ == "__main__":
-    
+
     docs = load_all_documents("data")
+
     store = FaissVectorStore("faiss_store")
-    #store.build_from_documents(docs)
+
+    # Vector store is already created, so we only load it
     store.load()
-    #print(store.query("What is attention mechanism?", top_k=3))
+
     rag_search = RAGSearch()
-    query = "What is attention mechanism?"
-    summary = rag_search.search_and_summarize(query, top_k=3)
+
+    query = "What is a data-intensive application?"
+
+    summary = rag_search.search_and_summarize(
+        query,
+        top_k=3
+    )
+
     print("Summary:", summary)
